1cb4caba9cb56f8fa0349e6370ae07ce430742cb
hakunlee <hakunlee@tmon.co.kr>
2017-12-06 17:14:45 +0900

    LocalDateTimeConstants 추가

---
+++ src/main/java/com/tmoncorp/common/util/constants/LocalDateTimeConstants.java
@@ -0,0 +1,16 @@
+package com.tmoncorp.common.util.constants;
+
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeConstants {
+    public static final String DATE_PATTERN = "yyyy-MM-dd";
+    public static final String TIME_PATTERN = "HH:mm:ss";
+    public static final String DATE_TIME_PATTERN = DATE_PATTERN + " " + TIME_PATTERN;
+
+    public static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat(DATE_TIME_PATTERN);
+
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN);
+    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(TIME_PATTERN);
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
+}
